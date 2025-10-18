85dcc14ddd7ff1b6167d789bec1462bd65210713
Amaan690 <mdamanullah690@gmail.com>
2025-10-18 18:35:12 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+function myfunction() {
+    console.log("Button was clicked!")
+    // Use querySelector to select the element by its class name
+    document.querySelector(".first").innerHTML = "Button was clicked!";
+}
