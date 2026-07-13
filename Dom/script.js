3eb55c312e3f8429888bd528738630414b6f77bd
Amaan690 <mdamanullah690@gmail.com>
2026-07-14 00:01:12 +0530

    updates

---
+++ Dom/script.js
@@ -0,0 +1,9 @@
+const paragrph = document.getElementById("para")
+paragrph.addEventListener("click", function(){
+    prompt("lorem")
+});
+
+const fullName = document.querySelector(".name");
+fullName.innerHTML = "Md.Amanullah"
+
+
